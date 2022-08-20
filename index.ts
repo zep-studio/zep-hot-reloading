71acd82cfc5b32bd4efc2c1161fb15f1cfacfbc6
Junho Yeo <i@junho.io>
2022-08-20 21:22:42 +0900

    Generate `.zepapp.zip` from generated script input

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
-import { publish } from './publish';
+import dedent from "dedent";
+import JSZip from 'jszip';
 
-publish();
+const appGeneratedScript = dedent`
+  var player, text;
+
+  App.onInit.Add(function () {
+    App.sayToAll('Hi, App Start!');
+  });
+  App.onSay.Add(function (player, text) {
+    for (var count = 0; count < 5; count++) {
+      App.sayToAll(text);
+      App.sayToAll(player);
+    }
+  });
+`;
+
+const generateZepapp = async () => {
+  const zip = new JSZip();
+  zip.file("main.js", appGeneratedScript);
+
+  const content = await zip.generateAsync({ type: "nodebuffer" });
+  // .zepapp.zip
+  console.log(content);
+};
+
+generateZepapp();
